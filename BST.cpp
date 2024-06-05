43196d2b7d2ec56c4b0c0af954df6bace83a92bd
Asim Poudel <ap03232822@student.ku.edu.np>
2024-06-05 10:46:58 +0545

    Add files via upload
    final draft

--- BST.cpp
+++ BST.cpp
@@ -1,60 +1,199 @@
+#include<iostream>
 #include "BST.h"
-#include <iostream>
 
 using namespace std;
 
-#include "BST.h"
-#include <iostream>
+class node { 
+    public:
+        int data; 
+        node* left; 
+        node* right; 
 
-using namespace std;
+        node(int value){
+            this->data=value;
+            this->left=NULL;
+            this->right=NULL;
+        }
 
-BST::BST(int size) {
-    tree = new int[size];
-    for (int i = 0; i < size; ++i)
-        tree[i] = -1;
+};
+
+class BSTLinkedList: public BST{
+    private:
+        node* root;
+    public:
+        BSTLinkedList(){
+            root=NULL;
+        }
+        bool isEmpty(){
+            return root==NULL;
+        }
+
+        void addBST(int value) {
+            if (isEmpty()) {
+                cout <<"Node Added with data:" << value << endl;
+                root = new node(value);
+                return;
+            }
+            addBST(root, value);
+            return;
+        }
+
+        node* addBST(node* temp, int value) {
+            if (temp == NULL) {
+                cout <<"Node Added with data:" << value << endl;
+                return new node(value);
+            }
+
+            if (value < temp->data) {
+                temp->left = addBST(temp->left, value);
+            } else if (value > temp->data) {
+                temp->right = addBST(temp->right, value);
+            }
+            return temp;
+        }
+
+        node* removeBST(node* temp, int value){
+            if (temp == NULL) {
+                cout << "Node with data:" << value << " not found." << endl;
+                return NULL;
+            }
+            if (temp->data == value){
+                // Node with 0 child
+                if (temp->left == NULL && temp->right == NULL) {
+                    delete temp;
+                    return NULL;
+                }
+                // Node with 1 child 
+                if (temp->left!=NULL && temp->right==NULL){
+                    temp = temp->left;
+                    delete temp->left;
+                    return temp ;
+                }
+                if (temp->right!=NULL && temp->left==NULL){
+                    temp = temp->right;
+                    delete temp->right;
+                    return temp;
+                }
+                // Node with 2 children
+                if (temp->left!=NULL && temp->right!=NULL){
+                    // Either find the maximum in the left subtree or minimum in the right subtree and replace by it and delete that node
+                    node* minNode = temp->right;
+                    while (minNode->left != NULL){
+                        minNode = minNode->left;
+                    }
+                    temp->data = minNode->data;
+                    temp->right=removeBST(temp->right, minNode->data);
+                }
+            } else if (value < temp->data) {
+                temp->left = removeBST(temp->left, value);
+            } else {
+                temp->right = removeBST(temp->right, value);
+            }
+        }
+
+        void removeBST(int value){
+            if (isEmpty()){
+                cerr<<"Binary Search Tree is Empty."<<std::endl;
+                return;
+            }
+            cout << "Node Deleted with data:" << value << endl;
+            removeBST(root, value);
+
+        }
+
+
+
+        bool searchBST(node* temp, int value){
+            if (temp == NULL) {
+                cout << "False (Not Found)" << endl;
+                return false;
+            }
+            if (temp->data == value) {
+                cout << "True (Found)" << endl;
+                return true;
+            }
+            if (value < temp->data) {
+                return searchBST(temp->left, value);
+            } else {
+                return searchBST(temp->right, value);
+            }
+        }
+
+        bool searchBST(int value){
+            cout<<""<<value<<" : ";
+            searchBST(root, value);
+        }
+
+  void InOrderTraversal(){
+        cout << "InOrder Traversal: ";
+            InOrder(root);
+        cout << endl;
+        }
+
+
+        void InOrder(node* node){
+            if(node == NULL){
+                return;
+            }
+            InOrder(node->left);
+            cout << node->data << " ";
+            InOrder(node->right);
+        }
+
+        void PreOrderTraversal(){
+            cout << "PreOrder Traversal: ";
+            PreOrder(root);
+            cout << endl;
+        }
+
+        void PreOrder(node* node){
+            if(node == NULL){
+                return;
+            }
+            cout << node->data << " ";
+            PreOrder(node->left);
+            PreOrder(node->right);
+        }
+
+        void PostOrderTraversal(){
+            cout << "PostOrder Traversal: ";
+            PostOrder(root);
+            cout << endl;
+        }
+
+        void PostOrder(node* node){
+            if(node == NULL){
+                return;
+            }
+            PostOrder(node->left);
+            PostOrder(node->right);
+            cout << node->data << " ";
+        }
+};
+
+
+int main(){
+    BSTLinkedList BT;
+    BT.addBST(10);
+    BT.addBST(20);
+    BT.addBST(5);
+    BT.addBST(15);
+    BT.addBST(25);
+    BT.addBST(2);
+    BT.addBST(7);
+    BT.addBST(12);
+    BT.addBST(37);
+    BT.addBST(6);
+    BT.InOrderTraversal();
+    BT.removeBST(5);
+    BT.removeBST(10);
+    BT.removeBST(20);
+    BT.PreOrderTraversal();
+    BT.searchBST(15);
+    BT.searchBST(20);
+    BT.addBST(69);
+    BT.addBST(1);
+    BT.PostOrderTraversal();
+    BT.isEmpty();
+    return 0;
 }
-
-BST::~BST() {
-    delete[] tree;
-}
-bool BST::isEmpty(){
-    if(tree[0]== -1)
-        return true;
-        return false;
-
-
-}
-
-void BST::addBST(int key,int value){
-    addBST(0, key,value);
-}
-void BST::addBST(int index,int key, int value){
-    if (index >= size) {
-        cout << "Tree is full, cannot insert more elements." << endl;
-        return;
-    }
-    if (tree[index] == -1) {
-        tree[index] = Data(key,value);
-        return;
-    }
-    if(value<tree[index])
-        addBST(2*index+1,value);
-    else
-        addBST(2*index+2,value);
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- BST.h
+++ BST.h
@@ -1,29 +1,18 @@
+#include<iostream>
 #ifndef BST_H
 #define BST_H
 
 
-struct Data {
-    int key;
-    int value;
 
-    Data(int key,int value): key(key),value(value){}
 
-};
 class BST{
-    protected:
-        int size;
-        Data *tree;
     public:
 
-        BST(int size);
-        virtual ~BST();
+        BST(){};
         virtual bool isEmpty()=0;
-        virtual void addBST(int index,Data Data)=0;
-        virtual void removeBST(int *keytoDelete)=0;
-        virtual bool searchBST(int *targetKey)=0;
-        //virtual void inorder=0();
-        //virtual void preorder=0();
-        //virtual void postorder=0();
+        virtual void addBST(int value)=0;
+        virtual void removeBST(int key)=0;
+        virtual bool searchBST(int Key)=0;
 
 };
 
